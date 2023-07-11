01b7ec0464166505645f5923f0735427b6a6624a
h584957 <54099686+h584957@users.noreply.github.com>
2023-07-11 18:57:08 +0200

    level1 version1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style/App.css';
+import Hotbar from './components/Hotbar';
+import Home from './page/Home.js';
+import Lv1 from './page/Lv1.js';
+import Lv2 from './page/Lv2.js';
+import Lv3 from './page/Lv3.js';
+import Lv4 from './page/Lv4.js';
+
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='root'>
+ 
+  <BrowserRouter>
+  <Hotbar/>
+      <Routes Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/lv1' element={<Lv1/>} />
+        <Route path='/lv2' element={<Lv2/>} />
+        <Route path='/lv3' element={<Lv3/>} />
+        <Route path='/lv4' element={<Lv4/>} />
+      </Routes>
+  </BrowserRouter>
+
+</div>
+
   );
 }
 
