c39dfd2526e8a916625807d483271c642fd44dae
h584957 <54099686+h584957@users.noreply.github.com>
2023-07-13 22:13:47 +0200

    level2 update

--- src/page/Lv2.js
+++ src/page/Lv2.js
@@ -4,12 +4,39 @@ import '../style/lv2/Todo.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { number } from 'mathjs';
+import { useRef, useState } from 'react';
 
 function Lv2() {
-    
-  
-  
+    const handleAddTodo = () => {
+      
+        if(newtoDos.trim() !== ""){
+            setTodos([...toDos, newtoDos]);     
+            setNewTodos("");
+            
+            inputRef.current.focus();
+
+        }
+    };
+
+    const inputRef = useRef(null);
+
+
+    const [toDos, setTodos] = useState([]);
+    const [newtoDos, setNewTodos] = useState("");
+    const handleInputChange = (e) => {
+      console.log("entered handleIn");
+      setNewTodos(e.target.value);
+    };
+
+    const handleEnter = (e) => {
+
+
+      if(e.key === "Enter"){
+        e.preventDefault();
+        handleAddTodo();
+      }
+    };
+   
   return (
       <div id='Page'>
         <div id='header'>
@@ -20,21 +47,24 @@ function Lv2() {
     
         <div id='lv2PageContainer'>
           <div id='todoBox'>
-            <input id='inputBar' autoComplete='off'/>
+            <input id='inputBar' onKeyDown={handleEnter} ref={inputRef} type='text' value={newtoDos} onChange={handleInputChange} autoComplete='off'/>
+            <button id ='addTodoBtn'onClick={handleAddTodo}>Add</button>
 
             <div id='todoListBox'>
-              <ul id='ul1'>
-              {}
-              </ul>
-
-            </div>
-          
-            <button id='checkTodo' className='todoBtn'>
+              <ul id='listContainer'>
+                {toDos.map((todo, index) => (<li id="list" key={index}>{todo}<button id='checkTodo' className='todoBtn'>
             <FontAwesomeIcon icon={faCheck} />
             </button>
             <button id='deleteTodo' className='todoBtn'>
             <FontAwesomeIcon icon={faXmark} />
-            </button>
+            </button> </li>))}                
+                
+                </ul>
+            
+
+            </div>
+          
+            
           </div>
           <div id='todoComplete'>
 
